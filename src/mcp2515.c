555525fe9cc834b56209f080e1ff94073369c20c
sss22213 <n0404.n0404@gmail.com>
2020-06-10 18:18:04 +0800

    dev

--- src/mcp2515.c
+++ src/mcp2515.c
@@ -3,43 +3,71 @@
 mcp2515_dev* new_mcp2515_dev(char *spidev_path,uint8_t mode,uint32_t speed)
 {
     mcp2515_dev* mcp2515_device = (mcp2515_dev*)malloc(sizeof(mcp2515_dev));
+    mcp2515_device->spi_dev = (spi_description*)malloc(sizeof(spi_description));
+    mcp2515_device->spi_dev->spidev_path = spidev_path;
+    mcp2515_device->spi_dev->mode = 3;
+    mcp2515_device->spi_dev->bits = 8;
+    mcp2515_device->spi_dev->speed = speed;
+    mcp2515_device->speed = speed;
+    mcp2515_device->mode = mode;
+    return mcp2515_device;
+}
+/*
+mcp2515_dev* new_mcp2515_dev(char *spidev_path,uint8_t mode,uint32_t speed)
+{
+    mcp2515_dev* mcp2515_device = (mcp2515_dev*)malloc(sizeof(mcp2515_dev));
     mcp2515_device->spidev_path = spidev_path;
     mcp2515_device->mode = mode;
     mcp2515_device->speed = speed;
     return mcp2515_device;
 }
+*/
 
-int mcp2515_initial(mcp2515_dev *mcp2515_device)
+int spi_initial(mcp2515_dev *mcp2515_device)
 {
     // read or set result
     int ret = 0;
 
     // open device
-    int fd = open(mcp2515_device->spidev_path, O_RDWR); 
+    int fd = open(mcp2515_device->spi_dev->spidev_path, O_RDWR); 
     if(fd < 0)return -1;
+    mcp2515_device->spi_dev->fd = fd;
 
-    
     // set CPOL => 1, CPOH => 1 
-    ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+    ret = ioctl(fd, SPI_IOC_WR_MODE, &(mcp2515_device->spi_dev->mode));
     if(ret == -1)return -2;
 
     // get CPOL => 1, CPOH => 1 
-    ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+    ret = ioctl(fd, SPI_IOC_RD_MODE, &(mcp2515_device->spi_dev->mode));
     if(ret == -1)return -3;
 
     //
-    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &(mcp2515_device->spi_dev->bits));
     if(ret == -1)return -4;
 
-    ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+    ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &(mcp2515_device->spi_dev->bits));
     if(ret == -1)return -5;
 
     // speed setting
-    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &(mcp2515_device->speed));
+    ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &(mcp2515_device->spi_dev->speed));
     if(ret == -1)return -6;
 
-    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &(mcp2515_device->speed));
+    ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &(mcp2515_device->spi_dev->speed));
     if(ret == -1)return -7;
 
     return 0;
+}
+
+
+int mcp2515_initial(mcp2515_dev *mcp2515_device)
+{
+  int ret = 0;
+  ret = spi_initial(mcp2515_device);
+  if(ret!=0)return ret;
+  return 0;
+}
+
+int mcp2515_send(mcp2515_dev *mcp2515_device,uint8_t *data,int length)
+{
+    
 }
--- src/mcp2515.h
+++ src/mcp2515.h
@@ -15,21 +15,151 @@
 #define TXB1CTRL 0x40
 #define TXB2CTRL 0x50
 
+// 
 #define TXRTSCTRL 0x0D
 
+// Standard Identifier Transmission buffer (MSByte)
+#define TXB0SIDH 0x31 
+#define TXB1SIDH 0x41
+#define TXB2SIDH 0x51
+
+// Standard Identifier Transmission buffer (LSByte)
+#define TXB0SIDL (TXB0SIDH + 0x01)
+#define TXB1SIDL (TXB1SIDH + 0x01)
+#define TXB2SIDL (TXB2SIDH + 0x01)
+
+// Extended Identifier 8 Reg Transmission buffer (MSByte)
+#define TXB0EID8 0x33
+#define TXB1EID8 0x43
+#define TXB2EID8 0x53
+
+// Extended identifier 0 Reg Transmission buffer (LSByte)
+#define TXB0EID0 (TXB0EID8 + 0x10) 
+#define TXB1EID0 (TXB1EID8 + 0x10) 
+#define TXB2EID0 (TXB2EID8 + 0x10)
+
+// Transmit buffer n data length code
+#define TXB0DLC 0x35
+#define TXB1DLC 0x45
+#define TXB2DLC 0x55
+
+// TRANSMIT BUFFER n DATA BYTE m REGISTER
+#define TXB0Dm 0x36
+#define TXB1Dm 0x46
+#define TXB2Dm 0x56
+
+// FILTER n STANDARD IDENTIFIER REGISTER HIGH
+#define RXF0SIDH 0x00
+#define RXF1SIDH 0x04
+#define RXF2SIDH 0x08
+#define RXF3SIDH 0x10
+#define RXF4SIDH 0x14
+#define RXF5SIDH 0x18
+
+// FILTER n STANDARD IDENTIFIER REGISTER LOW
+#define RXF0SIDL (RXF0SIDH + 0x01)
+#define RXF1SIDL (RXF1SIDH + 0x01)
+#define RXF2SIDL (RXF2SIDH + 0x01)
+#define RXF3SIDL (RXF3SIDH + 0x01)
+#define RXF4SIDL (RXF4SIDH + 0x01)
+#define RXF5SIDL (RXF5SIDH + 0x01)
+
+// FILTER n EXTENDED IDENTIFIER REGISTER HIGH
+#define RXF0EID8 0x02
+#define RXF1EID8 0x05
+#define RXF2EID8 0x0A
+#define RXF3EID8 0x12
+#define RXF4EID8 0x16
+#define RXF5EID8 0x1A
+
+// FILTER n EXTENDED 1 REGISTER LOW
+#define RXF0EID0 (RXF0EID8 + 0x01)
+#define RXF1EID0 (RXF1EID8 + 0x01)
+#define RXF2EID0 (RXF2EID8 + 0x01)
+#define RXF3EID0 (RXF3EID8 + 0x01)
+#define RXF4EID0 (RXF3EID8 + 0x01)
+#define RXF5EID0 (RXF4EID8 + 0x01)
+
+// MASK n STANDARD IDENTIFIER REGISTER HIGH 
+#define RXM0SIDH 0x20
+#define RXM1SIDH 0x24
+
+// MASK n STANDARD IDENTIFIER REGISTER LOW 
+#define RXM0SIDL (RXM0SIDH + 0x01)
+#define RXM1SIDL (RXM1SIDH + 0x01)
+
+// MASK n EXTENDED IDENTIFIER REGISTER HIGH
+#define RXM0EID8 0x22
+#define RXM1EID8 0x26
+
+// MASK n EXTENDED IDENTIFIER REGISTER LOW 
+#define RXM0EID0 (RXM0EID8 + 0x01)
+#define RXM1EID0 (RXM1EID8 + 0x01)
+
+// CONFIGURATION REGISTER
+#define CNF1 0x2A
+#define CNF2 0x29
+#define CNF3 0x28
+
+// TRANSMIT ERROR COUNTER REGISTER
+#define TEC 0x1C
+
+// RECEIVE ERROR COUNTER REGISTER
+#define REC 0x1D
+
+// ERROR FLAG REGISTER
+#define EFLG 0x2D
+
+// CAN INTERRUPT ENABLE REGISTER
+#define CANINTE 0x2B
+
+// CAN INTERRUPT FLAG REGISTER
+#define CANINTF 0x2C
+
+// CAN CONTROL REGISTER
+#define CANCTRL 0x0F
+
+// CAN STATUS REGISTER
+#define CANSTAT 0x0E
+
+// MCP2515 SPI CMD
+#define MCP2515_SPI_RESET 0x0C
+#define MCP2515_SPI_READ 0x03
+#define MCP2515_SPI_READ_RX_BUFFER 0x90
+#define MCP2515_SPI_WRITE 0x02
+#define MCP2515_SPI_LOAD_TX_BUFFER 0x40
+#define MCP2515_SPI_RTS 0x80
+#define MCP2515_SPI_READ_STATUS 0xA0
+#define MCP2515_SPI_RX_STATUS 0xB0
+#define MCP2515_SPI_BIT_MODIFY 0x05
+
 typedef struct mcp2515_dev mcp2515_dev;
+typedef struct spi_description spi_description;
 
 // spi set
+/*
 static uint8_t mode = 3;
 static uint8_t bits = 8;
+*/
 
 // defined mcp2515 feature
 typedef struct mcp2515_dev
 {
-    char *spidev_path;
+    spi_description* spi_dev;
     uint8_t mode;
     uint32_t speed;
 }mcp2515_dev;
+
+// defined spi description
+typedef struct spi_description
+{
+    char *spidev_path;
+    uint8_t fd;
+    uint8_t mode;
+    uint8_t bits;
+    uint32_t speed;
+}spi_description;
+
 
 // constructor
 mcp2515_dev* new_mcp2515_dev(char *,uint8_t,uint32_t);
@@ -37,6 +167,7 @@ mcp2515_dev* new_mcp2515_dev(char *,uint8_t,uint32_t);
 // open and initial mcp2515
 int mcp2515_initial(mcp2515_dev*);
 
-// mcp2515 address
+// Send Data to MCP2515
+int mcp2515_send(mcp2515_dev*,uint8_t*,int);
 
 #endif
